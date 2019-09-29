31bf74ebd8e90e5221f01f19bc070dcec446e8a3
Filippo Finke <filippo.finke@samtrevano.ch>
2019-09-29 15:38:06 +0200

    Added support to negative numbers

--- examples/bar.php
+++ examples/bar.php
@@ -5,10 +5,10 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 $title = "Orders";
-$values = [14, 10, 7];
-$labels = ["January", "February", "March"];
+$values = [-5, 10, 7, -10];
+$labels = ["January", "February", "March", "Test"];
 $size = 2;
-$colors = ["0;33", "0;32", "0;31"];
+$colors = ["0;33", "0;32", "0;31", "0;31"];
 
 $bar = new Charts\Bar($title, $values, $labels, $colors, $size);
 $bar->draw();
--- src/Bar.php
+++ src/Bar.php
@@ -28,22 +28,28 @@ class Bar extends Chart
         $colors = $this->getColors();
 
         $max = max($values);
+        $min = (min($values) < 0)?min($values):0;
+
+        $max_length = (strlen($max) > strlen($min))?strlen($max):strlen($min);
+
         $length = count($labels) - 1;
         foreach ($labels as $label) {
             $length += strlen($label);
         }
-        echo str_repeat(" ", strlen($max) + 1).str_pad($title, $length, " ", STR_PAD_BOTH).PHP_EOL;
-        for ($i = $max; $i > 0; $i--) {
-            echo str_pad($i, strlen($max))."|";
+        echo str_repeat(" ", $max_length + 1).str_pad($title, $length, " ", STR_PAD_BOTH).PHP_EOL;
+        for ($i = $max; $i >= $min; $i--) {
+            echo str_pad($i, $max_length)."|";
             foreach ($values as $index => $value) {
                 $label_length = strlen($labels[$index]) + 1;
-                if ($value >= $i) {
-                    if($colors != null) echo "\033[".$colors[$index]."m";
+                if($colors != null) echo "\033[".$colors[$index]."m";
+                if ($value >= $i && $value >= 0 && $i >= 0) {
                     echo str_pad(str_repeat("*", $size), $label_length, " ", STR_PAD_BOTH);
-                    if($colors != null) echo "\033[0m";
+                } else if($value < 0 && $i < 0 && $value <= $i) {
+                    echo str_pad(str_repeat("*", $size), $label_length, " ", STR_PAD_BOTH);
                 } else {
                     echo str_repeat(" ", $label_length);
                 }
+                if($colors != null) echo "\033[0m";
             }
             echo PHP_EOL;
         }
